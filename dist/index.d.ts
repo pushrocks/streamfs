b73fa40d76b302e5e34174512fa8328dab3ff1bb
Phil Kunz <gitlab@philkunz.com>
2017-09-19 16:07:14 +0200

    add createWritableStream()

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,3 +1,5 @@
 /// <reference types="node" />
 import { ReadStream } from 'fs';
+import { WriteStream } from 'fs';
 export declare let createReadStream: (pathArg: string) => ReadStream;
+export declare let createWriteStream: (pathArg: string) => WriteStream;
--- ts/index.ts
+++ ts/index.ts
@@ -1,7 +1,11 @@
 import * as plugins from './streamfs.plugins'
 
 import { ReadStream } from 'fs'
-
+import { WriteStream } from 'fs'
 export let createReadStream = (pathArg: string) => {
   return plugins.fs.createReadStream(pathArg)
+}
+
+export let createWriteStream = (pathArg: string) => {
+  return plugins.fs.createWriteStream(pathArg)
 }
